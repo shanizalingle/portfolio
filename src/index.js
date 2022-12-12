71b99bbfa0426c0893d2b3633a0528dc380255b5
Shaniza Lingle <shanizalingle@gmail.com>
2022-12-12 00:51:36 -0800

    add responsive navbar and containers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+
+// Navbar section
+
+function navbar(event) {
+  event.preventDefault();
+  document.querySelectorAll('.navbar__links').
+    forEach(link => {
+      if(link.href === window.location.href){
+        link.setAttribute('aria-current', 'page');
+      } else {
+        link.removeAttribute('aria-current', 'page');
+      }
+    })
+}
+
+window.addEventListener("scroll", navbar);
