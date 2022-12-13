e43420f49ece148524c3cea2286d90c91fcf911a
Shaniza Lingle <shanizalingle@gmail.com>
2022-12-12 19:10:11 -0800

    add scrolling feature to navbar

--- src/index.js
+++ src/index.js
@@ -3,17 +3,45 @@
 import './css/styles.css';
 
 // Navbar section
+const highlightMenu = () => {
+  const highlight = document.querySelector('.highlight');
+  const homeMenu = document.querySelector('#home-page');
+  const aboutMenu = document.querySelector('#about-page');
+  const projectsMenu = document.querySelector('#projects-page');
+  const contactMenu = document.querySelector('#contact-page');
+  let scrollPos = window.scrollY;
 
-function navbar(event) {
-  event.preventDefault();
-  document.querySelectorAll('.navbar__links').
-    forEach(link => {
-      if(link.href === window.location.href){
-        link.setAttribute('aria-current', 'page');
-      } else {
-        link.removeAttribute('aria-current', 'page');
-      }
-    })
-}
+  // adds 'highlight' class to my menu items
+  if (window.innerWidth > 960 && scrollPos < 450) {
+    homeMenu.classList.add('highlight');
+    aboutMenu.classList.remove('highlight');
+    projectsMenu.classList.remove('highlight');
+    contactMenu.classList.remove('highlight');
+    return;
+  } else if (window.innerWidth > 960 && scrollPos < 1000) {
+    aboutMenu.classList.add('highlight');
+    homeMenu.classList.remove('highlight');
+    contactMenu.classList.remove('highlight');
+    projectsMenu.classList.remove('highlight');
+    return;
+  } else if (window.innerWidth > 960 && scrollPos < 1600) {
+    projectsMenu.classList.add('highlight');
+    aboutMenu.classList.remove('highlight');
+    contactMenu.classList.remove('highlight');
+    homeMenu.classList.remove('highlight');
+    return;
+  } else if (window.innerWidth > 960 && scrollPos < 1900) {
+    contactMenu.classList.add('highlight');
+    projectsMenu.classList.remove('highlight');
+    homeMenu.classList.remove('highlight');
+    aboutMenu.classList.remove('highlight');
+    return;
+  }
 
-window.addEventListener("scroll", navbar);
+  if ((highlight && window.innerWIdth < 960 && scrollPos < 600) || highlight) {
+    highlight.classList.remove('highlight');
+  }
+};
+
+window.addEventListener('scroll', highlightMenu);
+window.addEventListener('click', highlightMenu);
